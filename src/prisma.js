202cb964aedba238f0b8fc8e7d4fd77cb4ae7311
Jane-Njoroge <njorogejwangui@gmail.com>
2025-06-14 18:21:19 +0300

    prisma file

--- src/prisma.js
+++ src/prisma.js
@@ -1,5 +1,17 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
